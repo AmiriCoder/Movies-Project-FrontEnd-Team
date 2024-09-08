a66cbbe325f2a5599ecc035505c71680fcd1601b
nava <nava.biglar@gmail.com>
2024-09-08 18:16:55 +0330

    Explore update

--- js/main.js
+++ js/main.js
@@ -53,17 +53,18 @@ $(document).ready(async function(){
 
     $('.owl-carousel').owlCarousel({
         
-        margin:150,
-       
+        loop:true,
+        margin: 20,
+        nav:true,
         responsive:{
             0:{
                 items:1
             },
             600:{
-                items:3
+                items:2
             },
             1000:{
-                items:5
+                items:4
             }
         }
     })
